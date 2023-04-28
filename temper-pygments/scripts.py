26372f39c32ea7fba5a4fe704c2e3ab3f9dc85e9
Tom <tom@temper.systems>
2023-04-28 11:41:06 -0700

    Add test script

--- temper-pygments/scripts.py
+++ temper-pygments/scripts.py
@@ -3,15 +3,24 @@ import subprocess
 
 
 def bundle():
-    pathlib.Path("dist").mkdir(exist_ok=True)
+    out_dir = "dist"
+    pathlib.Path(out_dir).mkdir(exist_ok=True)
     temper_out = "../temper-syntax/temper.out"
     subprocess.run(
         [
-            "stickytape",
+            *run("stickytape"),
             "./temper_pygments/__init__.py",
-            *("--output-file", "./dist/temper_pygments.py"),
+            *("--output-file", f"./{out_dir}/temper_pygments.py"),
             *("--add-python-path", f"{temper_out}/temper-syntax/py/"),
             *("--add-python-path", f"{temper_out}/temper-core/py/"),
             *("--add-python-path", "."),
         ]
     )
+
+
+def test():
+    subprocess.run([*run("python"), "-m", "unittest", "discover"])
+
+
+def run(command: str):
+    return ("poetry", "run", command)
